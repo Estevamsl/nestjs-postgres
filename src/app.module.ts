8cc532e117bb4f629aa8ea2fcf3740fcbf5e3dd1
Estevamsl <estevamsouzalaureth@gmail.com>
2022-06-25 19:43:44 -0300

    commit app.module.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesController } from './courses/courses.controller';
+import { ControllersController } from './controllers/controllers.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CoursesController],
+  controllers: [AppController, CoursesController, ControllersController],
   providers: [AppService],
 })
 export class AppModule {}
