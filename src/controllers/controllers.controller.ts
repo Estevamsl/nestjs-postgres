f9ab056c08d362eb8b4fafd95c7e5a3106a7b809
Estevamsl <estevamsouzalaureth@gmail.com>
2022-06-26 09:26:06 -0300

    id variado

--- src/controllers/controllers.controller.ts
+++ src/controllers/controllers.controller.ts
@@ -2,8 +2,14 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('controllers')
 export class ControllersController {
-  @Get('/')
+  @Get('/list')
   findAll(): string {
     return 'This action returns all controllers';
+  }
+
+  // Get para id variado
+  @Get('/:id')
+  findOne(id: number): string {
+    return `This action returns a #${id} controller`;
   }
 }
