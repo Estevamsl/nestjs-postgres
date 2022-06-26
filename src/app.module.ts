c8f3967a131389587e1dec410881cba5f9652466
Estevamsl <estevamsouzalaureth@gmail.com>
2022-06-26 10:27:51 -0300

    commit new file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesController } from './courses/courses.controller';
 import { ControllersController } from './controllers/controllers.controller';
+import { ServicesService } from './services/services.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CoursesController, ControllersController],
-  providers: [AppService],
+  providers: [AppService, ServicesService],
 })
 export class AppModule {}
