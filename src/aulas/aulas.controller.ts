754cd6dbe808d74b26c0ce40daa664f9290f68a4
Estevamsl <estevamsouzalaureth@gmail.com>
2022-06-27 14:52:47 -0300

    new CRUD

--- src/aulas/aulas.controller.ts
+++ src/aulas/aulas.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Delete, Body, Patch, Post } from '@nestjs/common';
 
 @Controller('aulas')
-export class AulasController {}
+export class AulasController {
+  constructor() {}
+
+  @Get('/')
+  findAll(): string {
+    return 'This action returns all aulas';
+  }
+
+  @Get('/:id')
+  findOne(@Param('id') id: number): string {
+    return `This action returns a #${id} aula`;
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') id: number): string {
+    return `This action removes a #${id} aula`;
+  }
+
+  @Patch('/:id')
+  update(@Param('id') id: number, @Body('name') body: string): string {
+    return `This action updates a #${id} aula`;
+  }
+
+  @Post()
+  create(@Body('name') body: string): string {
+    return `This action adds a new aula with name ${body}`;
+  }
+
+}
