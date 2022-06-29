dbe90528234179d2e53eac6eb314584168fc745f
Estevamsl <estevamsouzalaureth@gmail.com>
2022-06-29 15:06:46 -0300

    :heavy_minus_sign: removendo arquivo

--- src/module/escola/escola.controller.ts
+++
@@ -1,5 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-// @Controller('escola') //endpoint para receber requisições para que esse controller seja chamado para executar a ação desejada
-@Controller('escola')
-export class EscolaController {}
