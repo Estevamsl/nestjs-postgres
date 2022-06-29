232e53134d5734a90ee9ec7dd2ff0896834a104b
Estevamsl <estevamsouzalaureth@gmail.com>
2022-06-29 08:36:50 -0300

    :construction: Em progresso

--- src/module/escola/escola.controller.ts
+++ src/module/escola/escola.controller.ts
@@ -1,4 +1,5 @@
 import { Controller } from '@nestjs/common';
 
+// @Controller('escola') //endpoint para receber requisições para que esse controller seja chamado para executar a ação desejada
 @Controller('escola')
 export class EscolaController {}
