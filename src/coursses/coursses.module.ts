fc8b6968c8f5faa83d7cf0bb38aba3cd43b10a98
Estevamsl <estevamsouzalaureth@gmail.com>
2022-06-29 15:06:15 -0300

    :ok_hand: alteracoes de revisao de codigo

--- src/coursses/coursses.module.ts
+++ src/coursses/coursses.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Course } from 'src/entities/course.entity';
 import { CoursesController } from 'src/courses/courses.controller';
 import { ServicesService } from 'src/services/services.service';
 
 // os módulos são responsáveis por armazenar os componentes do projeto
 @Module({
+  imports: [TypeOrmModule.forFeature([Course])],
   controllers: [CoursesController],
   providers: [ServicesService],
 })
