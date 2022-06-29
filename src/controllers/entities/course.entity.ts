c7af54910164fd33d76ccc9b2f929b848e206357
Estevamsl <estevamsouzalaureth@gmail.com>
2022-06-29 08:37:55 -0300

    :sparkles: novo recurso

--- src/controllers/entities/course.entity.ts
+++ src/controllers/entities/course.entity.ts
@@ -10,3 +10,6 @@ export class Course { // classe que representa o curso que será criado e retorn
 }
 
 // entidade de curso que será usada para armazenar os dados do curso que será criado e retornado ao usuário (cursosService.create)
+// export class Course { // classe que representa o curso que será criado e retornado ao usuário (cursosService.create)
+// Será usado no service para armazenar os dados do curso que será criado e retornado ao usuário (cursosService.create)
+// Será usado em uma lista de cursos no service e com os seus respectivos métodos
