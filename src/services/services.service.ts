a63de2cbce93ba69c4d324627ebd94188c5e817f
Estevamsl <estevamsouzalaureth@gmail.com>
2022-06-29 15:07:09 -0300

    :ok_hand: alteracoes de revisao de codigo

--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -1,73 +1,62 @@
 import { Injectable, HttpCode, HttpStatus, HttpException } from '@nestjs/common';
-import { Course } from 'src/controllers/entities/course.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateCourseDto } from 'src/controllers/dto/create-course.dto';
+import { UpdateCourseDto } from 'src/controllers/dto/update-course.dto';
+import { Course } from 'src/entities/course.entity';
+import { Repository } from 'typeorm';
 
 // Os Services são responsáveis pelas regras de negócio da nossa aplicação
 @Injectable() // Injectable é um decorator que permite que o serviço seja injetado em outro serviço ou classe como um provider (dependência) geralmente usado para injetar dependências nos controllers
 export class ServicesService { // Injetar o Service no Controller para usar os métodos
-  private courses: Course[] = [
-    {
-      id: 1,
-      name: 'Angular',
-      description: 'Angular Course',
-      isPublished: true,
-      price: 1.99,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-    {
-      id: 2,
-      name: 'NodeJS',
-      description: 'NodeJS Course',
-      isPublished: true,
-      price: 2.99,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }
-  ];
-  
+  constructor(
+    @InjectRepository(Course)
+    private readonly courseRepository: Repository<Course>
+  ) {}
+
   // array de cursos que será usado para armazenar os cursos que serão criados e retornados ao usuário (cursosService.create)
   // Esse métodos irão manipular o array de cursos e retornar o curso que foi criado, consultado, consultado em parte, atualizado ou deletado (cursosService.create)
   // São responsáveis pela regra de negócio do curso (cursosService.create)
   // Aqui é o local onde você define as regras de negócio do curso (cursosService.create) e não no controller
-  findAll(): Course[] {
-    const course = this.courses;
-    if(!course) {
-      throw new HttpException('Course not found', HttpStatus.NOT_FOUND);
-    }
-    return course;
+  // findAll(): Course[] {
+  //   const course = this.courses;
+  //   if(!course) {
+  //     throw new HttpException('Course not found', HttpStatus.NOT_FOUND);
+  //   }
+  //   return course;
+  // }
+
+  findAll() {
+    return this.courseRepository.find();
   }
 
-  findOne(id: number): Course {
-    const course = this.courses.find(course => course.id === Number(id));
+  findOne(id: any) {
+    const course = this.courseRepository.findOne(id);
     if (!course) {
       throw new HttpException('Course not found', HttpStatus.NOT_FOUND);
     }
     return course;
   }
 
-  create(createCourseDto: any) {
-    this.courses.push(createCourseDto);
-    // course.id = this.courses.length + 1;
-    // course.createdAt = new Date();
-    // course.updatedAt = new Date();
-    // this.courses.push(course);
-    // return course;
+  create(createCourseDto: CreateCourseDto) {
+    const course = this.courseRepository.create(createCourseDto);
+    return this.courseRepository.save(course);
   }
 
-  update(id: number, course: Course): Course {
-    const courseToUpdate = this.findOne(id);
-    courseToUpdate.name = course.name;
-    courseToUpdate.description = course.description;
-    courseToUpdate.isPublished = course.isPublished;
-    courseToUpdate.price = course.price;
-    courseToUpdate.updatedAt = new Date();
-    return courseToUpdate;
+  async update(id: string, UpdateCourseDto: UpdateCourseDto) {
+   const course = await this.courseRepository.preload({ id: +id, ...UpdateCourseDto })
+   if (!course) {
+     throw new HttpException('Course not found', HttpStatus.NOT_FOUND);
+   }
+    return this.courseRepository.save(course);
   }
 
-  delete(id: any): Course {
-    const courseToDelete = this.findOne(id);
-    this.courses = this.courses.filter(course => course.id !== id);
-    return courseToDelete;
+  async delete(id: any) {
+    const courseToDelete = await this.courseRepository.findOne(id);
+    if (!courseToDelete) {
+      throw new HttpException('Course not found', HttpStatus.NOT_FOUND);
+    }
+    return this.courseRepository.remove(courseToDelete);
+    // this.courses = this.courses.filter(course => course.id !== id);
   }
 }
 
